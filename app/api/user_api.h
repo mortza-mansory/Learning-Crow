4ba4a8a42c78ecf4909f3a5db679b0d78102292a
mortza mansory <mortzamansory12@gmail.com>
2025-03-25 10:38:24 -0400

    Update user_api.h

--- app/api/user_api.h
+++ app/api/user_api.h
@@ -1,14 +1,13 @@
-#ifndef USER_SERVICE_H
-#define USER_SERVICE_H
+#ifndef USER_API_H
+#define USER_API_H
 
-#include <string>
 #include "../../include/crow_all.h"
 
-namespace user_service {
-    crow::response register_user(const std::string& username, const std::string& password, const std::string& job, const std::string& rule);
+namespace user_api {
+    crow::response register_user(const crow::request& req);
     crow::response get_user_details(int user_id);
     crow::response delete_user(int user_id);
-    crow::response update_user(int user_id, const std::string& username, const std::string& password, const std::string& job, const std::string& rule);
+    crow::response update_user(int user_id, const crow::request& req);
 }
 
-#endif // USER_SERVICE_H
+#endif // USER_API_H
